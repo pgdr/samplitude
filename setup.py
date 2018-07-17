d53b26e5c1f98e4d42c4c8b5e436c217417d74e7
Pål Grønås Drange <PGDR@statoil.com>
2018-07-17 13:16:48 +0200

    Add requirements numpy and Jinja2

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ setup(
     name='clidist',
     packages=['clidist'],
     version='0.0.1',
+    install_requires=[
+        'numpy>=1.11',
+        'Jinja2',
+        ],
     entry_points={
         'console_scripts': [
             'clidist = clidist:main',
