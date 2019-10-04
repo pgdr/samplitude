c5f48febebf38f40544e98442aa19557a53d8240
jeanpaul <jepebe@users.noreply.github.com>
2019-10-04 15:44:48 +0200

    Use list instead of cli to make code cleaner

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -14,38 +14,20 @@ class TestSamplitudeGenerators(SamplitudeTestCase):
                        '45')
 
     def test_normal(self):
-        self.asserts8e('normal(170, 10) | sample(3) | round | cli',
-                       """\
-167.178
-173.51
-164.402""")
-        self.asserts8e('gauss(170, 10) | sample(3) | round | cli',
-                       """\
-190.785
-169.526
-160.629""")
+        self.asserts8e('normal(170, 10) | sample(3) | round | list',
+                       '[167.178, 173.51, 164.402]')
+        self.asserts8e('gauss(170, 10) | sample(3) | round | list',
+                       '[190.785, 169.526, 160.629]')
 
     def test_uniform(self):
-        self.asserts8e('uniform(0, 42) | sample(3) | round | cli',
-                       """\
-41.848
-37.162
-17.254""")
-
+        self.asserts8e('uniform(0, 42) | sample(3) | round | list',
+                       '[41.848, 37.162, 17.254]')
 
     def test_exponential(self):
-        self.asserts8e('exponential(1/2.71828) | round | sample(3) | cli',
-                       """\
-15.274
-5.875
-1.438""")
-        self.asserts8e('poisson(1/2.71828) | round | sample(3) | cli',
-                       """\
-15.274
-5.875
-1.438""")
-
-
+        self.asserts8e('exponential(1/2.71828) | round | sample(3) | list',
+                       '[15.274, 5.875, 1.438]')
+        self.asserts8e('poisson(1/2.71828) | round | sample(3) | list',
+                       '[15.274, 5.875, 1.438]')
 
     def test_chi2(self):
         self.asserts8e('chi2(5) | sample(3) | round | len',
