bdda45f4a9bfc2dead2a482e8e16f05e5d21775b
jeanpaul <jepebe@users.noreply.github.com>
2019-10-04 15:42:44 +0200

    Add another rounding test

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -9,6 +9,12 @@ class TestSamplitudeFilters(SamplitudeTestCase):
         self.assertEqual('\n'.join(['0.{}'.format(_) for _ in range(7)]),
                          s8e('sin(0.1) | round(1) | sample(7) | cli'))
 
+    def test_rounding(self):
+        base = '[0.5, 0.25, 0.125, 0.0625, 0.03125] | round(%d) | list'
+        self.asserts8e(base % 1, '[0.5, 0.2, 0.1, 0.1, 0.0]')
+        self.asserts8e(base % 2, '[0.5, 0.25, 0.12, 0.06, 0.03]')
+        self.asserts8e(base % 3, '[0.5, 0.25, 0.125, 0.062, 0.031]')
+
     def test_scale(self):
         self.assertEqual('\n'.join(map(str, list(range(5, 15)))),
                          s8e('count() | sample(10) | shift(5) | cli'))
